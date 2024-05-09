e03f418a92912e2020f1774c3db26509860aab7c
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-05-09 22:40:57 +0500

    add main content in home page and make structure for about me

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <>
+    <div className='overflow-none'>
       <div className=' h-screen w-full main text-white'>
         <nav className=' p-4 flex  text-black-300 justify-around'>
           <div className='font-thin text-3xl hover:text-yellow-500 cursor-pointer'>UZAIR <span
@@ -21,7 +22,7 @@ function App() {
           </ul>
         </nav>
         <div className="flex w-full h-5/6 items-center justify-around gap-4 max-md:flex-col ">
-          <div className=" left  flex  flex-col justify-center  ">
+          <div className=" left w-3/5 flex  flex-col justify-center  ">
             <h2 className='leading-normal ml-12 text-3xl  text-start '>Aoa, My name is </h2>
             <h1 className='ml-12 text-5xl text-yellow-500'>Uzair Ahmad Khan</h1>
             <p className='leading-normal ml-12 text-4xl'>I'm a Web Designer <span className='text-yellow-500'>&</span> Frontend<br /> Developer</p>
@@ -30,18 +31,21 @@ function App() {
               <button className='px-4 py-2 m-2 rounded-3xl text-xl w-64 border-2 border-yellow-500 text-yellow-500'>Let's Talk</button>
             </div>
           </div>
-          <div className="right w-96 ">
-            <img src="./image/1.jpg" alt="imggg" />
+          <div className="right flex items-center justify-center w-2/5">
+            <img className='w-96 ' src="./image/1.jpg" alt="imggg" />
           </div>
         </div>
       </div>
 
-      <div className="About_me flex justify-around items-center main text-white w-full  h-screen ">
-        <div className="left">
-          <h1>About ME</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minima dolores laudantium ab alias explicabo ullam, mollitia possimus, sed dolore blanditiis eaque molestias. Rem quibusdam ab neque sequi excepturi totam possimus aut amet. Nemo, delectus necessitatibus nulla esse odit porro aperiam consequuntur unde sed.</p>
+      <div className="About_me flex justify-around  gap-4 max-md:flex-col items-center main text-white w-full  h-screen ">
+        <div className="left w-3/5 flex  flex-col  ">
+          <h1 className='leading-normal ml-12 text-3xl  text-start'>About <span className='text-yellow-500'>Me</span></h1>
+          <p className='leading-normal ml-12 text-xl w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sunt sapiente a enim aspernatur voluptatibus harum? Odit corporis quam dolorem repellat ab ducimus reiciendis molestiae non eum, dolor error quisquam quis consequatur reprehenderit quidem nobis distinctio velit fugit id iure. Repudiandae, praesentium sed eligendi consectetur hic quia animi ullam alias. Inventore odio aspernatur tempore praesentium nisi, voluptatum nobis consectetur? Quia.</p>
         </div>
-        <div className='right '></div>
+        <div className="right w-2/5 ">
+            <img className='w-96' src="./image/1.jpg" alt="imggg" />
+          </div>
+      </div>
       </div>
     </>
   )
