b583492a4557c1ac263e34eec0e4e982aa202b2c
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-05-12 00:49:52 +0500

    work on contact me page

--- src/App.jsx
+++ src/App.jsx
@@ -71,16 +71,16 @@ function App() {
         <div className="About_me contact w-full p-8 flex flex-col justify-center items-center">
           <h1 className="text-4xl">Contact <span className="text-yellow-500">Me</span></h1>
           <div className='flex gap-12 p-4'>
-            <div>
-              <img src="./Image/email_561127.png" alt="" />
+            <div className='flex flex-col justify-center items-center p-4 px-8 w-1/3'>
+              <img className="w-12 m-4 invert-[1]" src="./Image/email_561127.png" alt="" />
              <h1 className='text-lg'> uzairahmadkhanmalik@gmail.com</h1>
             </div>
-            <div>
-            <img src="./call_3178167.png" alt="" />
+            <div className='flex flex-col justify-center items-center p-4 px-8 w-1/3'>
+            <img className="w-12 m-4 invert-[1]" src="./Image/call_3178167.png" alt="" />
              <h1 className='text-lg'> +92 3135167976</h1>
             </div>
-            <div>
-            <img src="./Image/location_535239.png" alt="" />
+            <div className='flex flex-col justify-center items-center p-4 px-8 w-1/3'>
+            <img className="w-12 m-4 invert-[1]" src="./Image/location_535239.png" alt="" />
              <h1 className='text-lg'> Kamra,Pakistan</h1>
             </div>
           </div>
