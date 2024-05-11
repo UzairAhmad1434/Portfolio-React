9207b816813e9b51ca2076095a76378797f6ca18
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-05-11 23:39:44 +0500

    add content in service  page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <div className='overflow-none'>
+    <div className='text-white overflow-none'>
       <div className=' h-screen w-full main text-white'>
         <nav className=' p-4 flex  text-black-300 justify-around'>
           <div className='font-thin text-3xl hover:text-yellow-500 cursor-pointer'>UZAIR <span
@@ -27,7 +27,7 @@ function App() {
             <h1 className='ml-12 text-5xl text-yellow-500'>Uzair Ahmad Khan</h1>
             <p className='leading-normal ml-12 text-4xl'>I'm a Web Designer <span className='text-yellow-500'>&</span> Frontend<br /> Developer</p>
             <div className='w-auto p-4'>
-              <button className='px-4 py-2 ml-6 m-2 rounded-3xl text-xl w-64  bg-yellow-500 text-white hover:bg-transparent hover: border-yellow-500 hover: text-yellow-500'>Download CV</button>
+              <button className='px-4 py-2 ml-6 m-2 rounded-3xl text-xl w-64  bg-yellow-500 text-white hover:bg-transparent '>Download CV</button>
               <button className='px-4 py-2 m-2 rounded-3xl text-xl w-64 border-2 border-yellow-500 text-yellow-500'>Let's Talk</button>
             </div>
           </div>
@@ -46,6 +46,28 @@ function App() {
             <img className='w-96' src="./image/1.jpg" alt="imggg" />
           </div>
       </div>
+
+      <div className="main services w-full h-screen flex flex-col items-center justify-center p-8">
+        <h1 className='text-4xl p-4 '>My <span className='text-yellow-500'>Services</span></h1>
+        <div className="card flex gap-4 p-8">
+          <div className='w-60 About_me p-2 rounded-md '>
+            <h1 className='text-yellow-500 text-center text-2xl' >HTML/CSS</h1>
+            <p className='text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
+          </div>
+          <div className='w-60 About_me p-2 rounded-md'>
+            <h1 className='text-yellow-500 text-center text-2xl' >Javascript</h1>
+            <p className='text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
+          </div>
+          <div className='w-60 About_me p-2 rounded-md'>
+            <h1 className='text-yellow-500 text-center text-2xl' >React</h1>
+            <p className='text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
+          </div>
+          <div className='w-60 About_me p-2 rounded-md'>
+            <h1 className='text-yellow-500 text-center text-2xl' >Webflow</h1>
+            <p className='text-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
+          </div>
+        </div>
+        </div>
       </div>
     </>
   )
