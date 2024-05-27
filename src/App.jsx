b5a7097f5a8c01c8db9b264e59dcb000b41e9544
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-05-27 23:43:59 +0500

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ function App() {
             <h1 className='ml-12 text-5xl text-yellow-500'>Uzair Ahmad Khan</h1>
             <p className='leading-normal font-light ml-12 text-4xl'>I'm a Web Designer <span className='text-yellow-500'>&</span> Frontend<br /> Developer</p>
             <div className='w-auto p-4'>
-              <button className='px-4 py-4 ml-6 m-2  text-2xl w-64  bg-yellow-500 text-white hover:bg-transparent border-2 border-yellow-500 transition-all duration-300 ease-in-out hover:text-yellow-500'>Download CV</button>
-              <button className='px-4 py-4 m-2  text-2xl w-64 border-2 border-yellow-500  text-yellow-500 hover:bg-yellow-500 hover:text-white transition-all duration-300 ease-in-out'>Let's Talk</button>
+              <button className='px-4 py-4 ml-6 m-2  text-2xl w-56  bg-yellow-500 text-white hover:bg-transparent border-2 border-yellow-500 transition-all duration-300 ease-in-out hover:text-yellow-500'>Download CV</button>
+              <button className='px-4 py-4 m-2  text-2xl w-56 border-2 border-yellow-500  text-yellow-500 hover:bg-yellow-500 hover:text-white transition-all duration-300 ease-in-out'>Let's Talk</button>
             </div>
           </div>
           <div className="right flex items-center justify-center w-2/5">
@@ -41,7 +41,7 @@ function App() {
         <div className="left w-3/5 flex  flex-col items-start ">
           <h1 className='leading-normal ml-12 text-4xl  text-start'>About <span className='text-yellow-500'>Me</span></h1>
           <p className='leading-normal ml-12 text-lg font-light w-3/4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, aut accusantium nobis quam, possimus consequatur quae, quaerat perferendis eum voluptate dolores iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sunt sapiente a enim aspernatur voluptatibus harum? Odit corporis quam dolorem repellat ab ducimus reiciendis molestiae non eum, dolor error quisquam quis consequatur reprehenderit quidem nobis distinctio velit fugit id iure. Repudiandae, praesentium sed eligendi consectetur hic quia animi ullam alias. Inventore odio aspernatur tempore praesentium nisi, voluptatum nobis consectetur? Quia.</p>
-          <button className='px-4 py-2 m-4 ml-12  text-xl w-64 border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white transition-all transition-300 ease-in-out'>Let's Talk</button>
+          <button className='px-4 py-2 m-4 ml-12  text-xl w-44 border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white transition-all transition-300 ease-in-out'>Let's Talk</button>
         </div>
         <div className="right flex items-center justify-center w-2/5 ">
             <img className='w-96' src="./image/aboutimg.jpg" alt="imggg" />
@@ -90,6 +90,8 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div className="footer">Copyright $c</div>
       </div>
     </>
   )
