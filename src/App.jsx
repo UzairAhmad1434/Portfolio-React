ee9a191f7b894ec1a8fdabba04423f51288f6283
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-05-31 15:16:22 +0500

    cards component

--- src/App.jsx
+++ src/App.jsx
@@ -50,27 +50,11 @@ function App() {
         <div className="main services w-full  flex flex-col  justify-center p-8">
           <h1 className='text-4xl p-4 ml-24 text-center'>My <span className='text-yellow-500'>Services</span></h1>
           <div className="cards flex flex-wrap gap-4 p-8 justify-center">
-            <Card card='hello' />
-            <div className='w-80 card p-8 m-2'>
-              <h1 className='text-yellow-400 text-center text-2xl' >Database</h1>
-              <p className='text-md text-center font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
-            </div>
-            <div className='w-80 card p-8 m-2'>
-              <h1 className='text-yellow-400 text-center text-2xl' >HTML / CSS</h1>
-              <p className='text-md text-center font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
-            </div>
-            <div className='w-80 card p-8 m-2'>
-              <h1 className='text-yellow-400 text-center text-2xl' >Javascript</h1>
-              <p className='text-md text-center font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
-            </div>
-            <div className='w-80 card p-8 m-2' >
-              <h1 className='text-yellow-400 text-center text-2xl' >React</h1>
-              <p className='text-md font-light text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
-            </div>
-            <div className='w-80 card p-8 m-2'>
-              <h1 className='text-yellow-400 text-center text-2xl' >Webflow</h1>
-              <p className='text-md font-light text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id possimus corrupti, aliquid laudantium iste explicabo molestiae odio eius, praesentium ipsum. Repellendus mollitia magni enim. Maxime incidunt optio non aliquam accusamus, vel iste.</p>
-            </div>
+            <Card card='Database' />
+            <Card card='HTML / CSS' />
+            <Card card='Javascript' />
+            <Card card='React' />
+            <Card card='Webflow' />
           </div>
         </div>
         <div className="About_me contact w-full p-8 h-fit flex flex-col justify-center items-center">
