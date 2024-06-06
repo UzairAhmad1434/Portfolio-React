2144602bcb604cb6ae9d7f53ad9cbe611ae2dc22
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-06-06 23:52:08 +0500

    manu svg

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <div className='text-white overflow-none'>
         <div className=' h-screen w-full main text-white'>
           <NAV />
+
           <ul className='About_me hidden list-none  flex-col pt-2 justify-center items-center gap-6 text-xl font-light cursor-pointer'>
             <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>Home</li>
             <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>About</li>
@@ -93,7 +94,7 @@ function NAV() {
         <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>Services</li>
         <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>My Skills</li>
         <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>My Work</li>
-        <li className='hover:border-b-2 border-yellow-500 transition-all duration-100 ease-in-out'>Contact Me</li>
+        <img src="./Image/menu.svg" alt="" />
       </ul>
     </nav>
   )
