9f53dcea1d96ff3bc6f13cb347cd1ccd16d75436
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-06-28 19:18:19 +0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ Card.propTypes = {
 
 function NAV({ isOpen, setIsOpen }) {
   return (
-    <nav className="About_me p-4 flex main text-black-300 justify-around text-white">
+    <nav className="About_me p-4  flex sticky top-0 w-full main text-black-300 justify-around text-white">
       <div className="text-4xl flex items-center hover:text-yellow-500 cursor-pointer transition-all duration-500 ease-in-out font-light">
         UZAIR{" "}
         <span className="border-none text-yellow-500 font-extrabold hover:text-white">
@@ -191,6 +191,9 @@ function AboutMe() {
         <h1 className="leading-normal text-4xl text-start">
           About <span className="text-yellow-500">Me</span>
         </h1>
+        <h2 className="text-start text-3xl text-yellow-500 font-bold">
+          Front-end Developer
+        </h2>
         <p className="leading-normal text-lg font-light w-full">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
           soluta suscipit quisquam recusandae, incidunt est vitae distinctio
@@ -216,7 +219,7 @@ function AboutMe() {
 function ContactMe() {
   return (
     <div className="About_me md:p-8 p-4 flex lg:flex-row-reverse flex-col contact w-full justify-center items-center">
-      <div className="w-full flex flex-col items-center h-fit p-0 sm:p-4">
+      <div className="w-full flex flex-col items-center h-fit gap-4 px-2 py-4 sm:p-4">
         <Contact name="+923135167976" source="./Image/phone.svg" />
         <Contact name="Kamra,Pakistan" source="./Image/location.svg" />
         <Contact
@@ -224,13 +227,15 @@ function ContactMe() {
           source="./Image/email.svg"
         />
       </div>
-      <div className="flex hover justify-center md:p-8 p-4 rounded-xl w-full  flex-col items-center m-0 lg:mx-8 main gap-4">
-        <h1 className="text-yellow-500 text-3xl">Let's Work Together</h1>
+      <div className="flex hover justify-center md:p-8 py-4  rounded-xl w-full  flex-col items-center m-0 lg:mx-8 main gap-4">
+        <h1 className="text-yellow-500 sm:text-3xl text-xl sm:text-start m-0 sm:m-4 text-center">
+          Let's Work Together
+        </h1>
         <p className="full hover:none font-light">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et
           temporibus qui magni saepe. Maxime.
         </p>
-        <div className="display text-lg flex flex-wrap gap-4 sm:gap-8 items-center justify-center">
+        <div className="display sm:text-lg text-md flex flex-wrap gap-4 sm:gap-8 items-center justify-center">
           <Input type="text" placeholder="First Name" class="half" />
           <Input type="text" placeholder="Last Name" class="half" />
           <Input type="text" placeholder="Phone Number" class="half" />
@@ -275,8 +280,8 @@ Input.propTypes = {
 
 function Contact(props) {
   return (
-    <div className=" w-full flex sm:flex-row flex-col items-center p-2">
-      <div className="bg-yellow-500 m-2 sm:mx-2">
+    <div className="About_me w-full flex sm:flex-row flex-col items-center rounded-md p-2">
+      <div className="bg-yellow-500 rounded-md m-2 sm:mx-2">
         <img className="p-2 w-12 m-2 invert-[1]" src={props.source} alt="" />
       </div>
       <p className="sm:text-xl  text-lg text-yellow-500 font-medium">
