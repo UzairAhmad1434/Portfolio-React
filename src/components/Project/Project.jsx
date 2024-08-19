a8b09d976300059a0c33205e22e610bc8ba5e88d
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-08-19 22:00:33 +0500

    --

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,43 +6,53 @@ const Project = () => {
           My <span className="text-yellow-500">Projects</span>
         </h1>
         <div className=" flex flex-wrap flex-row gap-4 items-center p-4 justify-center ">
-          <div className="bg-[url('/Image/weather.jpg')] main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
+          <div className="bg-weather bg-cover p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
+            <h1 className="text-2xl text-yellow-500 font-medium">
+              Weather App
+            </h1>
             <p className="text-md text-center ">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
               aperiam harum
             </p>
           </div>
-          <div className="main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
+          <div className="bg-study bg-cover p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
+            <h1 className="text-2xl text-yellow-500 font-medium">StudyBuddy</h1>
             <p className="text-md text-center ">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
               aperiam harum
             </p>
           </div>
-          <div className="main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
+          <div className="bg-cover rounded-md bg-currency w-72 h-48 ">
+            <div className="hover:bg-gradient-to-r from-sky-500 to-indigo-500 flex p-2  items-center justify-center flex-col w-72 h-48 rounded-md">
+              <h1 className="text-2xl text-yellow-500 font-medium">
+                Currency Converter
+              </h1>
+              <p className="text-md text-center ">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
+                aperiam harum
+              </p>
+            </div>
+          </div>
+          <div className="bg-calculator bg-cover p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
+            <h1 className="text-2xl text-yellow-500 font-medium">Calculator</h1>
             <p className="text-md text-center ">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
               aperiam harum
             </p>
           </div>
-          <div className="main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
+          <div className="bg-store bg-cover p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
+            <h1 className="text-2xl text-yellow-500 font-medium">
+              Ecommerce Store
+            </h1>
             <p className="text-md text-center ">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
               aperiam harum
             </p>
           </div>
-          <div className="main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
-            <p className="text-md text-center ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
-              aperiam harum
-            </p>
-          </div>
-          <div className="main p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
-            <h1 className="text-xl text-yellow-500">StudyBuddy</h1>
+          <div className="bg-tictac bg-cover p-2 flex items-center justify-center flex-col w-72 h-48 rounded-md">
+            <h1 className="text-2xl text-yellow-500 font-medium">
+              Tic Tac Toe
+            </h1>
             <p className="text-md text-center ">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
               aperiam harum
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        weather: "url('/Image/weather.jpg')",
+        study: "url('/Image/studybuddy.jpg')",
+        calculator: "url('/Image/Calculator.jpg')",
+        currency: "url('/Image/currency.jpg')",
+        tictac: "url('/Image/tictac.jpg')",
+        store: "url('/Image/u_store.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
